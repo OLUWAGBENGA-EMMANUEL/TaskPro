3f1415bdaa0240ebb1e3190af44fab6b6b3e3281
OLUWAGBENGA EMMANUEL ARIYO <ariyooe2000@gmail.com>
2025-03-04 18:00:54 +0000

    Test

--- app/src/main/java/uk/ac/tees/scedt/mad/d3740020/taskpro/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/scedt/mad/d3740020/taskpro/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TaskProTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Welcome to TaskPro",
+                        name = "Task Pro",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
